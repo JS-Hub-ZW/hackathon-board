1d4911d02cff62407f2cb168aa381d69bfc653a6
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-20 00:20:06 +0200

    fixed tailwind config, cleaned up index

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {Text, Stack} from '@chakra-ui/react'
+import { Text, Stack } from "@chakra-ui/react";
 
 export default function Home() {
-    
   return (
-      <>
-     <Text>
-        Hello world testing the chakra-ui
-      </Text>
-
-      </>
-  )
-       
-  
+    <>
+      <Text>Hello world testing the chakra-ui</Text>
+    </>
+  );
 }
