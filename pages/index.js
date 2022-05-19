d4955e93020d2a0be1cc59795cb010c30eb0c9b4
Richson Simbabure <richsonsimbabure@gmail.com>
2022-05-19 18:13:36 -0400

    added the environment setup

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import {Text, Stack} from '@chakra-ui/react'
+
+export default function Home() {
+    
+  return (
+      <>
+     <Text>
+        Hello world testing the chakra-ui
+      </Text>
+
+      </>
+  )
+       
+  
+}
