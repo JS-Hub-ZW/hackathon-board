086343e83576ba00e5f1da5e0d9cf4a707497ea5
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-05-29 21:34:21 +0200

    Added auth pages

---
+++ pages/auth/login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoginCard from '../../components/auth/login'
+
+export default function Login() {
+  return (
+    <LoginCard/>
+  )
+}
---
+++ pages/auth/register.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import RegisterCard from '../../components/auth/register'
+
+export default function Register() {
+  return (
+    <RegisterCard/>
+  )
+}
