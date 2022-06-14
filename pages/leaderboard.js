d6cc478d7aad60c186f35d5e346dd2ac4034ac9f
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-06-14 13:11:57 +0200

    Added leaderboard and linked it

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,16 +1,12 @@
 import {Text} from '@chakra-ui/react'
+import LeaderBoard from '../components/leaderboard'
 import Section from '../components/section'
 
 const Leaderboard = () => {
     return (
      <>
      <Section delay={0.2}>
-       <Text
-        fontSize='6xl'
-        align="center"
-        >
-       🏆Create me they need to know who is leading
-       </Text>
+       <LeaderBoard/>
      </Section>
      </>
     )
