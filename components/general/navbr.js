509df608abe0a331a2868d06eefee7d36cd6f458
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-06-14 13:12:39 +0200

    Change source to our work

--- components/general/navbr.js
+++ components/general/navbr.js
@@ -118,7 +118,7 @@ const Navbar = (props) => {
             style={{ gap: 4 }}
             pl={2}
           >
-            Source
+            Our Work
           </LinkItem>
           <LinkItem
             href={"auth/login"}
