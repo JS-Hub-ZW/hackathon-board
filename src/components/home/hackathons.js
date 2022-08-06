dc2fdd8508af3bd32647de91d62092107c4899c2
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-06 18:14:39 +0200

    Modify hackathon image

--- src/components/home/hackathons.js
+++ src/components/home/hackathons.js
@@ -20,13 +20,10 @@ import {
 } from '@chakra-ui/react';
 
 
-import { FiShoppingCart } from 'react-icons/fi';
-import { ThumbUpIcon } from '../../icons/thumbsUp';
-
 const data = {
   isNew: true,
   imageURL:
-    'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+    'https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/ebb4a9afeaaa463c23a6b5127e3ce224',
   name: 'Wayfarer Classic',
   price: 4.5,
   rating: 4.2,
@@ -77,6 +74,8 @@ function HackathonCard() {
                     alt={`Picture of ${data.name}`}
                     roundedTop="lg"
                     roundedBottom="lg"
+                    height={40}
+                    width={"100%"}
                   />
 
                   {/* Like Icon  */}
