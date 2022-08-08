f7ea9e534eed81fc485e0b724122bccce82597d5
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-08 23:02:48 +0200

    link contributers

--- pages/about.js
+++ pages/about.js
@@ -14,36 +14,7 @@ import EmailSubs from "../src/components/emailsubs";
 import TeamCards from "../src/components/about/teamCard";
 import {SimpleGrid} from "@chakra-ui/react";
 import {GridItem} from "@chakra-ui/react";
-
-const CONTRIBUTERS = [
-    {
-        name: "Richson Simbabure",
-        image: "https://avatars.githubusercontent.com/u/76937134?v=4",
-        role: "Software Developer",
-        link: "https://github.com/RSimmz98"
-    }, {
-        name: "George Chakama",
-        image: "https://avatars.githubusercontent.com/u/37905683?v=4",
-        role: "Software Developer",
-        link: "https://github.com/gtchakama"
-    }, {
-        name: "Dante Midzi",
-        image: "https://avatars.githubusercontent.com/u/34038162?v=4",
-        role: "Half Stack Developer",
-        link: "https://github.com/Psypher1"
-    }, {
-        name: "Ashley Hwacha",
-        image: "https://avatars.githubusercontent.com/u/22978304?v=4",
-        role: "Software Developer",
-        link: "https://github.com/ashleyhwacha"
-    }, {
-        name: "Takunda Madechangu",
-        image: "https://avatars.githubusercontent.com/u/42911769?v=4",
-        role: "Full Stack Developer",
-        link: "https://github.com/takumade"
-    }
-
-]
+import CONTRIBUTERS from "../src/data/contributers";
 
 const About = () => {
     return (
