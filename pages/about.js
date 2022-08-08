4fb053ded097bf3315a4a84a2e3d18f81c6b0468
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-08 23:15:15 +0200

    Import and use Purpose component

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,7 @@ import {
 } from "@chakra-ui/react";
 import Section from "../src/components/section";
 import Contributers from "../src/components/about/contributers";
+import Purpose from "../src/components/about/purpose";
 
 
 const About = () => {
@@ -25,21 +26,7 @@ const About = () => {
 
        <Box mb={10}>
 
-       <Heading>Purpose</Heading>
-        <Text  mt={5}>The purpose of Hackthon Board is to display hackathons that people can see and  participate. Its kind boring
-          hear that some people just won a hackathon but you didnt hear about it. We want to help people to find hackathons 
-          and hack stuff. Thats the purpose of Hackthon Board.
-        </Text>
-
-        
-
-       </Box>
-       <Box mb={10}>
-        <Heading>Target Audience</Heading>
-        <Text  mt={3}>
-          Hackthon Board displays hackathons in Zimbabwe and mainly targets Zimbabwe hackers so they showcase their skills. However we may
-          display hackathons in other countries and target other countries hackers. 
-        </Text>
+           <Purpose/>
         </Box>
 
 
