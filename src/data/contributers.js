1e52b67ac4cb7343d2f6467b6173f7ae0ee8d1e3
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-08 23:02:39 +0200

    Move contribs list to data

---
+++ src/data/contributers.js
@@ -0,0 +1,31 @@
+const CONTRIBUTERS = [
+    {
+        name: "Richson Simbabure",
+        image: "https://avatars.githubusercontent.com/u/76937134?v=4",
+        role: "Software Developer",
+        link: "https://github.com/RSimmz98"
+    }, {
+        name: "George Chakama",
+        image: "https://avatars.githubusercontent.com/u/37905683?v=4",
+        role: "Software Developer",
+        link: "https://github.com/gtchakama"
+    }, {
+        name: "Dante Midzi",
+        image: "https://avatars.githubusercontent.com/u/34038162?v=4",
+        role: "Half Stack Developer",
+        link: "https://github.com/Psypher1"
+    }, {
+        name: "Ashley Hwacha",
+        image: "https://avatars.githubusercontent.com/u/22978304?v=4",
+        role: "Software Developer",
+        link: "https://github.com/ashleyhwacha"
+    }, {
+        name: "Takunda Madechangu",
+        image: "https://avatars.githubusercontent.com/u/42911769?v=4",
+        role: "Full Stack Developer",
+        link: "https://github.com/takumade"
+    }
+
+]
+
+export default CONTRIBUTERS
