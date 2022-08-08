98578f6d93bb66dbe06f28fb832c860c0123683b
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-08 23:14:51 +0200

    Move purspose functionality to Purspose component

---
+++ src/components/about/purpose.js
@@ -0,0 +1,30 @@
+
+import {
+    Heading,
+    Box,
+    Button,
+    Text,
+} from "@chakra-ui/react";
+import Section from "../src/components/section";
+
+function Purpose() {
+    return (  <Box>
+        <Heading>Purpose</Heading>
+        <Text  mt={5}>The purpose of Hackthon Board is to display hackathons that people can see and  participate. Its kind boring
+          hear that some people just won a hackathon but you didnt hear about it. We want to help people to find hackathons 
+          and hack stuff. Thats the purpose of Hackthon Board.
+        </Text>
+
+        
+
+       </Box>
+       <Box mb={10}>
+        <Heading>Target Audience</Heading>
+        <Text  mt={3}>
+          Hackthon Board displays hackathons in Zimbabwe and mainly targets Zimbabwe hackers so they showcase their skills. However we may
+          display hackathons in other countries and target other countries hackers. 
+        </Text>
+    </Box>);
+}
+
+export default Purpose;
