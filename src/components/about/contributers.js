fe2a948d9e4236c3943dec99649801aa99399f66
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-08 23:12:10 +0200

    Move contributers to contributer component

---
+++ src/components/about/contributers.js
@@ -0,0 +1,39 @@
+
+import {
+    Heading,
+    Box,
+    Button,
+    Text,
+} from "@chakra-ui/react";
+import {SimpleGrid} from "@chakra-ui/react";
+import {GridItem} from "@chakra-ui/react";
+import CONTRIBUTERS from "../../data/contributers";
+import ContributerCard from "./contributerCard";
+
+
+function Contributers() {
+    return ( 
+    
+    <Box><Heading>Contributers</Heading>
+
+    <Text mt={5} mb={10} align={"center"}>This project wouldnt come this far without
+        these lovely heroes. I mean if you need someone to do your work, Please get in
+        touch with these guys. They are awesome!.
+    </Text>
+
+    <SimpleGrid
+        columns={{
+        base: 1,
+        sm: 2,
+        md: 3,
+        lg: 4
+    }}
+        spacing="12">
+        {CONTRIBUTERS.map(contributer => (
+            <GridItem><ContributerCard contributer={contributer}/></GridItem>
+        ))}
+
+    </SimpleGrid> </Box>);
+}
+
+export default Contributers;
