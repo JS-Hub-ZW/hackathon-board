b0d6ba1415c3d5978297671df80fd25ccc61c7f8
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-08 23:27:59 +0200

    Link contribute and say hello components

--- pages/contact.js
+++ pages/contact.js
@@ -1,171 +1,50 @@
-import { Text, 
+import {
+  Text,
   Heading,
   Link,
-   Box,
+  Box,
   Center,
   useColorModeValue,
   Stack,
   SimpleGrid,
-  Image,
-  Button,
-  } from '@chakra-ui/react'
 
-  import {GoMarkGithub} from 'react-icons/go'
-  import {MdWavingHand} from 'react-icons/md'
+} from '@chakra-ui/react'
+import Contribute from '../src/components/contact/contribute'
+import SayHello from '../src/components/contact/sayHello'
 
-
-  import NextLink  from 'next/link'
-  
 
 
 import Section from '../src/components/section'
- 
-const HELLO_IMAGE = 'https://images.unsplash.com/photo-1535551951406-a19828b0a76b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=866&q=80';
-const CONTRIBUTE_IMAGE = "https://images.unsplash.com/photo-1483817101829-339b08e8d83f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=804&q=80"
 
 const Contact = () => {
-  return(
-  <>
-    <Section delay={0.3}>
-      <Heading
-        fontSize='6xl'
-        align="center"
+  return (
+    <>
+      <Section delay={0.3}>
+        <Heading
+          fontSize='6xl'
+          align="center"
           padding={6}
         >
-       Contact Us
-       </Heading>
+          Contact Us
+        </Heading>
 
-    <Center py={12}>
-      <SimpleGrid
-          columns={[1,2]}
-          gap={8}>
-       <Box
-        role={'group'}
-        p={6}
-        maxW={'500px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${HELLO_IMAGE})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={445}
-            objectFit={'cover'}
-            src={HELLO_IMAGE}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Say Hi
-          </Heading>
-          <Stack direction={'row'}>
-            <Text fontSize={'sm'} align={'center'}>
-              Do you want to make an enquiry? Or perhaps you want to share some memes? Please  come through....
-                   
-             </Text>
-              
-            </Stack>
-                   <NextLink href="mailto:jshubzw@gmail.com" target="_blank" passHref isExternal>
-                   
-                   <Button  colorScheme={"green"} rightIcon={<MdWavingHand/>}>Get In Touch</Button>
-                   </NextLink>
-        </Stack>
-      </Box>
-                <Box
-        role={'group'}
-        p={6}
-        maxW={'500px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${CONTRIBUTE_IMAGE})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={"full"}
-            objectFit={'cover'}
-            src={CONTRIBUTE_IMAGE}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Contribute 
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontSize={'sm'} align={'center'}>
-                   Hackathon Board is far from perfect and its free and open source. We could use some extra hands inorder to 
-                   improve it. 
+        <Center py={12}>
+          <SimpleGrid
+            columns={[1, 2]}
+            gap={8}>
+            <SayHello useColorModeValue={useColorModeValue} />
+            <Contribute useColorModeValue={useColorModeValue} />
+          </SimpleGrid>
+        </Center>
 
 
-              
-             </Text>
-              
-            </Stack>
-            <Link target="_blank" href='https://github.com/JS-Hub-ZW/hackathon-board' passHref isExternal>
-                   <Button colorScheme="green" rightIcon={<GoMarkGithub/>}>Hack the Board!</Button>
-                   </Link>
-        </Stack>
-      </Box>
-      </SimpleGrid>
-    </Center>
 
-    
+        <Text
+          padding={8}></Text>
+      </Section>
 
-          <Text 
-            padding={8}></Text>
-  </Section>
- 
-  </>
+    </>
   )
-  }
+}
 export default Contact
 
