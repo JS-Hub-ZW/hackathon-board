c68bb68746ffdbb7cc983b67937f5550edece80c
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-09 08:53:50 +0200

    Remove unusued imports and import main and side details compomnents

--- src/pages/detail/[id].jsx
+++ src/pages/detail/[id].jsx
@@ -1,15 +1,10 @@
 import { Divider, Flex, HStack, SimpleGrid, Wrap } from "@chakra-ui/react";
-import { Heading } from "@chakra-ui/react";
 import { Stack, Box, Image, Text } from "@chakra-ui/react";
-import DescriptionItem from "../../components/hackathonDetails/descriptionItem";
-import DetailItem from "../../components/hackathonDetails/detailItem";
+import HackathonMainDetail from "../../components/hackathonDetails/mainDetails";
+import HackathonSideDetails from "../../components/hackathonDetails/sideDetails";
 import Section from "../../components/section";
 
-import { CashIcon } from "../../src/icons/cashIcon";
-import { EyeIcon } from "../../src/icons/eyeIcon";
-import { GlobeIcon } from "../../src/icons/globeIcon";
-import { UsersIcon } from "../../src/icons/users";
-import { titleCase } from "../../src/utils/general.utils";
+
 
 
 
@@ -25,47 +20,13 @@ export default function Detail({hackathon}) {
             </HStack>
 
             <Stack direction={["column", "row"]} mt="12" spacing="6">
-                <Box width={{ lg: "70%", md:"70%", sm: "100%"}}>
+                <HackathonMainDetail  
+                    hackathon={hackathon}
+                />
 
-                    <Heading>{hackathon.name}</Heading>
-
-                    <Box mt={10}>
-                        <DescriptionItem heading="Description" isRichText={true}>{hackathon.description}</DescriptionItem>
-                        <DescriptionItem heading="Requirements" isRichText={true}>{hackathon.requirements}</DescriptionItem>
-                        <DescriptionItem heading="Rules" isRichText={true}>{hackathon.rules}</DescriptionItem>
-                        <DescriptionItem heading="Selection Criteria" isRichText={true}>{hackathon.selection_criteria}</DescriptionItem>
-                        <DescriptionItem heading="Prizes" isRichText={true}>{hackathon.prizes}</DescriptionItem>
-                    </Box>
-
-                </Box>
-
-                <Box width={{ md:"30%",sm:"100%", lg:"30%"}}>
-                    <Box mb={'6'}>
-                        <Heading fontSize={"1.5em"}>Details</Heading>
-                        <HStack spacing="12" mt="3">
-                            <DetailItem icon={<GlobeIcon boxSize={6} />} text={hackathon.type ? titleCase(hackathon.type) : ""}/>
-                            <DetailItem icon={<EyeIcon boxSize={6} />} text={hackathon.setting ? titleCase(hackathon.setting) : "Public"} />
-                   
-                        </HStack>
-                        <HStack spacing={"12"} mt="3">
-                            <DetailItem icon={<CashIcon boxSize={6} />} text={hackathon.prices} />
-                            <DetailItem icon={<UsersIcon boxSize={6} />} text={hackathon.participants} />
-                        </HStack>
-                    </Box>
-
-                    <Heading fontSize={"1.5em"}>Sponsors</Heading>
-                    <Wrap spacing="12px" p="3" >
-                        {
-                            hackathon.sponsors?.map((sponsor, index) => {
-                                return (
-                                    <Image key={index} maxWidth={"74"} src={sponsor.imageURL} />
-                                )
-                            })
-                        }
-                 
-                    </Wrap>
-
-                </Box>
+               <HackathonSideDetails  
+                    hackathon={hackathon}
+                />
             </Stack>
 
         </Section>
