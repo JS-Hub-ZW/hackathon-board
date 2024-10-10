0191b52f801e4391a9b3fb4338984e453951d92c
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-10 13:39:30 +0200

    Add try catch to catch errors

--- src/lib/octocat.js
+++ src/lib/octocat.js
@@ -1,23 +1,30 @@
 // Octokit.js
 
-import { Octokit } from "octokit"
+import { Octokit } from "octokit";
 
 // https://github.com/octokit/core.js#readme
 const octokit = new Octokit({
-    auth: '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'
-  })
+  auth: "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",
+});
 
+const owner = "JS-Hub-ZW";
+const repo = "hackathon-board";
 
-const owner = 'JS-Hub-ZW'
-const repo = 'hackathon-board'
- 
-export const getContributers = async () =>  {
-  return await octokit.request(`GET /repos/${owner}/${repo}/contributors`, {
-    owner: owner,
-    repo: repo,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-}
-
+export const getContributers = async () => {
+  try {
+    let result = await octokit.request(
+      `GET /repos/${owner}/${repo}/contributors`,
+      {
+        owner: owner,
+        repo: repo,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    
+    return result.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
