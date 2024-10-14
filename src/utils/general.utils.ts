66b23d03a40328d30ee52a5cf076b2f8563b96fc
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-14 05:57:20 +0200

    Generate UUID

--- src/utils/general.utils.ts
+++ src/utils/general.utils.ts
@@ -37,3 +37,12 @@ export const processName = (name:string, limit=30) => {
 export const titleCase = (str:string) => {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
+
+export const generateUUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0; // Random number from 0 to 15
+      const v = c === 'x' ? r : (r & 0x3 | 0x8); // Generate values
+      return v.toString(16); // Convert to hexadecimal
+    });
+  }
+  
