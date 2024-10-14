f39b28eaf915108ed1b1ee3ef3349db6998edfb4
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-14 05:55:11 +0200

    finish test

--- cypress/e2e/about.cy.ts
+++ cypress/e2e/about.cy.ts
@@ -6,7 +6,13 @@ describe('template spec', () => {
     cy.contains("Target Audience")
     cy.contains("Contributors")
 
+    cy.get(".chakra-button").should("contain.text", "Follow")
 
+    cy.contains("Full Stack Developer")
+    cy.contains('Follow').click();
 
+    cy.origin("https://github.com", () => {
+      cy.url().should("contain", "https://github.com/")  
+    })
   })
 })
