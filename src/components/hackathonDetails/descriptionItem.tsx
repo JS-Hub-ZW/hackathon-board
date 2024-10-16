efafd9b6cb7bf9fcc633cba503d0248fa1b64826
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-16 22:26:54 +0200

    Remove log

--- src/components/hackathonDetails/descriptionItem.tsx
+++ src/components/hackathonDetails/descriptionItem.tsx
@@ -4,7 +4,7 @@ import { SerializeToJSX } from "../general/SerializeToJSX";
 
 export default function DescriptionItem({ heading, isRichText, value }:{heading:string, value:string, isRichText:boolean}) {
 
-    console.log("The Text: ", children)
+
     return (
         <Stack mt={"3"} mb={"6"}>
             <Heading fontSize={"1.5em"}>{heading}</Heading>
