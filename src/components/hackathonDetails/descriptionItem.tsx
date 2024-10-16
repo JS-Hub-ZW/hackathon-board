0756fc9d38806e69bfafc5287f69e2868998bf5c
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-16 22:36:18 +0200

    Add list items

--- src/components/hackathonDetails/descriptionItem.tsx
+++ src/components/hackathonDetails/descriptionItem.tsx
@@ -1,15 +1,31 @@
-import { Stack, Text } from "@chakra-ui/react"
+import { OrderedList, Stack, Text, ListItem } from "@chakra-ui/react"
 import { Heading } from "@chakra-ui/react"
 import { SerializeToJSX } from "../general/SerializeToJSX";
 
-export default function DescriptionItem({ heading, isRichText, value }:{heading:string, value:string, isRichText:boolean}) {
+export function DescriptionItem({ heading, isRichText, text }:{heading:string, text:string, isRichText:boolean}) {
 
 
     return (
         <Stack mt={"3"} mb={"6"}>
             <Heading fontSize={"1.5em"}>{heading}</Heading>
     
-            <Text>{value ? value.toString() : ""}</Text>
+            <Text>{text ? text.toString() : ""}</Text>
+        </Stack>
+    )
+}
+
+export function ListItems({ heading, isRichText, items }:{heading:string, items:string[], isRichText:boolean}) {
+
+
+    return (
+        <Stack mt={"3"} mb={"6"}>
+            <Heading fontSize={"1.5em"}>{heading}</Heading>
+    
+            <OrderedList>
+                { items.map(item =>  <ListItem>{item}</ListItem>)}
+ 
+
+</OrderedList>
         </Stack>
     )
 }
