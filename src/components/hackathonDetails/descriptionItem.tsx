39d5b735bb5cac9433fa3a347408144c0a953392
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-16 22:26:37 +0200

    Remove children prop

--- src/components/hackathonDetails/descriptionItem.tsx
+++ src/components/hackathonDetails/descriptionItem.tsx
@@ -2,14 +2,14 @@ import { Stack, Text } from "@chakra-ui/react"
 import { Heading } from "@chakra-ui/react"
 import { SerializeToJSX } from "../general/SerializeToJSX";
 
-export default function DescriptionItem({ heading, isRichText, children }:{heading:string, isRichText:boolean, children:any}) {
+export default function DescriptionItem({ heading, isRichText, value }:{heading:string, value:string, isRichText:boolean}) {
 
     console.log("The Text: ", children)
     return (
         <Stack mt={"3"} mb={"6"}>
             <Heading fontSize={"1.5em"}>{heading}</Heading>
     
-            {isRichText && children && children.length > 0 && typeof(children) == 'object' ? SerializeToJSX(children) : <Text></Text> }
+            <Text>{value ? value.toString() : ""}</Text>
         </Stack>
     )
 }
